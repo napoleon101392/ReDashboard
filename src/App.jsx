454a83761b47d03e6d8fec95f70fce875eedba44
napoleon101392 <napoleon101392@gmail.com>
2022-10-31 15:30:52 +0800

    add drawer theme dark

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ import Table from './partials/Table';
 function App() {
   return (
     <div className="App">
-      <Alert message="Success" status="success" />
-      <Alert message="Something went wrong" status="error" />
-      <Table />
+      <div class="drawer">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+          <Alert message="Success" status="success" />
+          <Alert message="Something went wrong" status="error" />
+          <Table />
+
+          <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
+        </div> 
+        <div class="drawer-side">
+          <label for="my-drawer" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
