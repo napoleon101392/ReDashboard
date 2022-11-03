b2803032a8e4f1aa03dad08b15e40366acedd804
napoleon101392 <napoleon101392@gmail.com>
2022-11-03 18:39:55 +0800

    add form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,13 @@ export default function Contact() {
     return (
         <div className="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
             <div className="flex items-center">
-                <h1>Contact Us form here</h1>
+                <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSckZmneXsUFY1zz9EDT6ThLcQGt3DDAcClN2XMjn-dMgBzq-w/viewform?embedded=true" 
+                width="1024" 
+                height="1204"
+                frameborder="0" 
+                marginheight="0" 
+                marginwidth="0">Loading…</iframe>
             </div>
         </div>
     )
