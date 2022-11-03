97287dab1dc4be1d0050c5f2b93f4bc53be99e38
napoleon101392 <napoleon101392@gmail.com>
2022-11-03 18:54:47 +0800

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import Table from './partials/Table';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='container mx-auto'>
       <Navigation />
+      <Footer />
     </div>
   );
 }
